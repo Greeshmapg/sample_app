a343d6bca6d8bc761851708d40bb4daee3212d04
user4 <greeshma@spericorn.com>
2017-09-15 11:41:15 +0530

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
+
 
   get 'password_resets/index'
 
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
 
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
